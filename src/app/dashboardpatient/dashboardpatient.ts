42f2c88f6a1a537728262774b4fcd209af2bb2da
ademuliadi <ademuliadi@email.com>
2025-07-14 15:24:13 +0800

    buat halaman dashboard pasien

---
+++ src/app/dashboardpatient/dashboardpatient.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardpatient',
+  imports: [],
+  templateUrl: './dashboardpatient.html',
+  styleUrl: './dashboardpatient.css'
+})
+export class Dashboardpatient {
+
+}
